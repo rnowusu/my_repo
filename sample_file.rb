13ea26aea23a728b7f20a2bb84480df83040e8bd
Ryan <rnowusu@gwu.edu>
2018-03-22 00:16:29 -0400

    This is my first commit ever! It's littington.

---
+++ sample_file.rb
@@ -0,0 +1,12 @@
+def hello
+puts "hello, world"
+end
+
+def greet(name)
+  puts "Hello, #{name}"
+end
+
+hello
+
+
+greet("jeff")
---
+++ sample_file2.rb
@@ -0,0 +1,8 @@
+def nums_to_1(num)
+  num.times do |i|
+    puts i
+    puts "Num above is the current number"
+  end
+end
+
+nums_to_1(8)
